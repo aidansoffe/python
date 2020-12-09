1c1314bfdf2bf1f47f4822e6f97bc1f403494b5d
aidansoffe <aidansoffe@gmail.com>
2020-12-08 21:36:09 -0800

    mid

--- flask-madlibs/stories.py
+++ flask-madlibs/stories.py
@@ -1,12 +1,14 @@
-"""Madlibs Stories."""
 
 class Story:
     """Madlibs story.
 
-    To  make a story, pass a list of prompts, and the text
-    of the template.
+    To  make a story, pass a code, a title, a list of prompts,
+    and the text of the template.
 
-        >>> s = Story(["noun", "verb"],
+        >>> s = Story(
+        ...     "simple",
+        ...     "A Simple Tale",
+        ...     ["noun", "verb"],
         ...     "I love to {verb} a good {noun}.")
 
     To generate text from a story, pass in a dictionary-like thing
@@ -17,9 +19,11 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, code, title, words, text):
         """Create story with words and template text."""
 
+        self.code = code
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -34,11 +38,28 @@ class Story:
         return text
 
 
+
 # Here's a story to get you started
 
 
 story = Story(
+    "earth", "dragon", "climb", "silly", "berries",
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
+
+story1 = Story(
+    "stones", "eat",
+    ["plural_noun", "verb"],
+    """ OMG! OMG! I love to {verb} with {plural_noun}"""
+)
+story2 = Story(
+    "strong", "kindergarden",
+    ["adjective", "place"],
+    """ I like being {adjective} when I am in {place} """
+)
+
+
+# Make dict of {code:story, code:story, ...}
+stories = {s.code: s for s in [story1, story2]}
