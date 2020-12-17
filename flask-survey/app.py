8bc2435a0aa2d2baa727aca7301261a7fb08d6f0
aidansoffe <aidansoffe@gmail.com>
2020-12-16 20:43:34 -0800

    still

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -2,5 +2,15 @@ from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension 
 from random import randint, choice, sample
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'oh so secret'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+responses = []
+
+""" Create a route for a single question"""
+
+@app.route('/question')
+def ask_question():
+  return render_template('questions.html')
